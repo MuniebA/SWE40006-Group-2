e5f59a5d475f7ada44d076da9d019733f74b60d4
Rafid AL JAWAD <134399476+RafidAlJawad1@users.noreply.github.com>
2025-05-21 02:15:34 +0800

    Update main.tf

--- main .tf
+++ main .tf
@@ -71,7 +71,7 @@ data "aws_ami" "al2023" {
 
 
 resource "aws_security_group" "web" {
-  name   = "tf-web-sg"
+  name   = "tf-web-sg-${random_pet.suffix.id}"
   vpc_id = data.aws_vpc.default.id
 
   # HTTP 80
@@ -119,9 +119,10 @@ resource "tls_private_key" "ssh" {
   rsa_bits  = 4096
 }
 
+resource "random_pet" "suffix" {}
 
 resource "aws_key_pair" "generated" {
-  key_name   = "tf-generated-key"
+  key_name   = "tf-generated-${random_pet.suffix.id}"
   public_key = tls_private_key.ssh.public_key_openssh
 }
 
@@ -131,19 +132,20 @@ resource "local_file" "private_key" {
   file_permission = "0600"
 }
 
-
+data "aws_ssm_parameter" "al2023_minimal" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-minimal-kernel-default-x86_64"
+}
 
 resource "aws_instance" "web" {
-  ami                         = data.aws_ami.al2023.id
-  instance_type               = var.instance_type
-  subnet_id                   = data.aws_subnets.default.ids[0]
-  vpc_security_group_ids      = [aws_security_group.web.id]
+  ami           = data.aws_ssm_parameter.al2023_minimal.value
+  instance_type = var.instance_type
+  subnet_id     = data.aws_subnets.default.ids[0]
+  vpc_security_group_ids = [aws_security_group.web.id]
   associate_public_ip_address = true
-
   key_name = aws_key_pair.generated.key_name
 
   root_block_device {
-    volume_size = 4
+    volume_size = 8  # Adjust as needed; must be >= minimal AMI's snapshot size
     volume_type = "gp3"
   }
 
